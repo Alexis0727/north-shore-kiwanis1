d37fc9d8c7084dc2a5414c055b10e865cef048bc
Alexis0727 <atriola@lions.molloy.edu>
2025-12-13 00:47:52 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,12 @@
+export default function About() {
+  return (
+    <main className="page">
+      <h1>About Us</h1>
+      <p>
+        Kiwanis International is a global organization of volunteers committed
+        to serving children and strengthening communities.
+      </p>
+    </main>
+  );
+}
 
