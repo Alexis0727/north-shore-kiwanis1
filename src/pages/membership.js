876e56e729fc185d6f23189faf0919086469e2f3
Alexis0727 <atriola@lions.molloy.edu>
2025-12-13 00:03:43 -0500

    Add Membership component to pages

---
+++ src/pages/membership.js
@@ -0,0 +1,11 @@
+export default function Membership() {
+  return (
+    <main className="page">
+      <h1>Membership</h1>
+      <p>
+        Becoming a Kiwanis member means making a lasting difference in the lives
+        of children and families.
+      </p>
+    </main>
+  );
+}
