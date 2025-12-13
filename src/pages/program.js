68a1ee4c879d6cb91a9f59456e85ba9cf8bf5ba8
Alexis0727 <atriola@lions.molloy.edu>
2025-12-13 00:04:19 -0500

    Add Programs component to display program information

---
+++ src/pages/program.js
@@ -0,0 +1,11 @@
+export default function Programs() {
+  return (
+    <main className="page">
+      <h1>Programs</h1>
+      <p>
+        Our programs focus on youth leadership, education, and community support
+        initiatives.
+      </p>
+    </main>
+  );
+}
