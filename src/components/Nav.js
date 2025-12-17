558aed5b85f152fdb7ba945d1ef280a6eb0c2a8c
Alexis0727 <atriola@lions.molloy.edu>
2025-12-17 00:22:17 -0500

    Remove unused export statements from Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -29,14 +29,3 @@ const Nav = () => {
     </nav>
   );
 };
-
-export default Nav;
-=======
-        <nav>
-          <ul>
-            <li><HashLink to = "/">Home</HashLink></li>
-            <li><HashLink to = "/about">About Organization</HashLink></li>
-            <li><HashLink to = "/contact">Contact Us</HashLink></li>
-          </ul>
-        </nav>
->>>>>>> Stashed changes
